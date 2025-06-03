6198033fabd91421c4f6a4d1f31b604c5df4f792
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-06-03 16:16:42 +0200

    [UPD] Fetch movie detail

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,37 +1,35 @@
 import React, { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import filmsArray from "../assets/data/filmsArray";
+// import filmsArray from "../assets/data/filmsArray";
 import { useEffect } from "react";
 import RecensioneCard from "../components/RecensioneCard";
+import axios from "axios";
 
 const MovieDetail = () => {
   const { id } = useParams();
-  const [movies, setMovies] = useState(filmsArray);
   const [selectedMovie, setSelectedMovie] = useState({});
-  const navigate = useNavigate();
 
-  const fetchMovie = (id) => movies.find((movie) => movie.id === parseInt(id));
+  const fetchMovie = () => {
+    axios
+      .get(`http://localhost:3000/api/movies/${id}`)
+      .then((resp) => setSelectedMovie(resp.data))
+      .catch((error) => console.log(error));
+  };
 
-  useEffect(() => {
-    setSelectedMovie(fetchMovie(id));
-  }, []);
+  useEffect(fetchMovie, []);
 
   return (
     <>
       <div className="row">
         <div className="col-12 col-md-6 col-lg-4">
-          <img
-            src={`/${selectedMovie.immagine}`}
-            alt=""
-            className="img-fluid"
-          />
+          <img src={`${selectedMovie.image}`} alt="" className="img-fluid" />
         </div>
         <div className="col-12 col-md-6 col-lg-8">
-          <h1> {selectedMovie.titolo}</h1>
-          <p>{selectedMovie.trama}</p>
-          <p>{selectedMovie.anno}</p>
-          <p>{selectedMovie.regista}</p>
+          <h1> {selectedMovie.title}</h1>
+          <p>{selectedMovie.abstract}</p>
+          <p>{selectedMovie.release_year}</p>
+          <p>{selectedMovie.director}</p>
         </div>
       </div>
       <div className="row">
