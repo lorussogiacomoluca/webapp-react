a98403353954e77ec80e5612400fde6d817f93ec
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-06-05 14:39:53 +0200

    [UPD] Import Loader in DefaultLayout

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import GlobalContext from "../context/GlobalContext";
+import Loader from "../components/Loader";
 
 const DefaultLayout = () => {
   const { isLoading } = useContext(GlobalContext);
