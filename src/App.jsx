f6ec8836e8469ccc7fd31d1ac9b09e885b0d3bf6
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-06-05 14:36:36 +0200

    [UPD] Wrap App.jsx Router in GlobalContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Homepage from "./pages/Homepage";
 import MovieDetail from "./pages/MovieDetail";
+import GlobalContext from "./context/GlobalContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="/movie/:id" element={<MovieDetail />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContext.Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<Homepage />} />
+              <Route path="/movie/:id" element={<MovieDetail />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   );
 }
